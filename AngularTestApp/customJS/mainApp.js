3d3052f908ac0e6fdcfb71985b7ccdc435289391
Rahul Punjabi <rahul.punjabi@hotmail.com>
2016-01-12 00:09:55 +0530

    update working, alert bug removed

--- AngularTestApp/customJS/mainApp.js
+++ AngularTestApp/customJS/mainApp.js
@@ -22,9 +22,14 @@
 .controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
 
     this.showCreateAlert = false;
+    var scope = this;
 
+    this.resetFlag = function () {
+        this.showCreateAlert = false;
+    }
+
+    //getAll
     this.loadGrid = function () {
-        var scope = this;
         $http.get('api/Students')
             .success(function (data) {
               scope.students = data;
@@ -32,15 +37,48 @@
           });
     }
 
+    //getSingle
+    this.getStudent = function (studentID) {
+        $http.get('api/Students/' + studentID)
+        .success(function (data) {
+            scope.student = data;
+            scope.student.EnrollmentDate = new Date(data['EnrollmentDate']);
+            console.log(scope.student.EnrollmentDate);
+            console.log(data);
+        });
+
+    }
+
+    //Create
     this.createStudent = function (student) {
-        console.log("here");
-        var scope = this;
         $http.post('api/Students', student)
         .success(function () {
             console.log("Created");
             scope.loadGrid();
             scope.showCreateAlert = true;
         })
+    }
+
+    //Update
+    this.editStudent = function (studentID,student) {
+        $http.put('api/Students/' + studentID, student)
+        .success(function () {
+            console.log("Updated");
+            scope.loadGrid();
+            scope.showCreateAlert = true;
+        })
+    }
+
+    //Delete
+    this.deleteStudent = function (studentID) {
+        $http.delete('api/Students/' + studentID)
+        .success(function (data){
+            console.log(data);
+            scope.loadGrid();
+            scope.showCreateAlert = true;
+        }
+         
+        )
     }
 
 }])
